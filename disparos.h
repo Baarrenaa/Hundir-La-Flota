b19a788a04e65f84c7bc5f0c3f3dc3b608d401a5
Baarrenaa <josemanuel.barrenadiaz@alum.uca.es>
2025-03-19 17:30:10 +0100

    Update disparos.h

--- disparos.h
+++ disparos.h
@@ -1,1 +1,9 @@
+#ifndef DISPAROS_H
+#define DISPAROS_H
 
+#include "tableros.h"
+
+void realizarDisparo(Tablero *tablero, int x, int y);
+int verificarImpacto(const Tablero *tablero, int x, int y);
+
+#endif
