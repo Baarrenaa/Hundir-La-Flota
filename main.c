9c750c3fac5f07138923944138d9444586888199
Baarrenaa <josemanuel.barrenadiaz@alum.uca.es>
2025-03-19 17:30:38 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "configuracion.h"
+#include "tableros.h"
+#include "barcos.h"
+#include "disparos.h"
+#include "interfaz.h"
+#include "validaciones.h"
+#include "archivos.h"
 
+int main() {
