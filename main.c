87cbeda069ce0380b1521f427ae5f8e0eaf1ae39
Robelit <roberto11lm2018@gmail.com>
2025-03-19 17:40:15 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,10 @@
+int main() {
 
+    Tablero* tableroj1 = crear_tablero(tamano);
+    Tablero* tableroj2 = crear_tablero(tamano);
+    mostrar_tablero(tableroj1);
+    mostrar_tablero(tableroj2);
+
+
+    return 0;
+}
