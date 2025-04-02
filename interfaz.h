a0257bd3bc5a144f407f8c3c1ec2e3ad8adf6abe
Robelit <roberto11lm2018@gmail.com>
2025-04-02 13:59:03 +0200

    Update interfaz.h

--- interfaz.h
+++ interfaz.h
@@ -1,1 +1,10 @@
+#ifndef INTERFAZ_H
+#define INTERFAZ_H
 
+#include "configuracion.h"
+
+void mostrar_menu_principal(void);
+void mostrar_configuracion(Configuracion* config);
+void mostrar_tableros(Jugador* jugador_actual, Jugador* oponente);
+
+#endif
