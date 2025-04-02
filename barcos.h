c9fc80c51fc09f2cc357f9ccd2cc3ec6ea0367a4
Robelit <roberto11lm2018@gmail.com>
2025-04-02 13:54:27 +0200

    Update barcos.h

--- barcos.h
+++ barcos.h
@@ -1,13 +1,1 @@
-#ifndef BARCOS_H
-#define BARCOS_H
 
-typedef struct {
-    char nombre[21];
-    char id;
-    int tamano;
-} Barco;
-
-Barco* cargar_barcos(const char* filename);
-void liberar_barcos(Barco* barcos);
-
-#endif
