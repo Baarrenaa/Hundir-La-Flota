2198a5fb58ac3231ed180ebec5bc284066f4a080
Robelit <roberto11lm2018@gmail.com>
2025-04-02 14:08:45 +0200

    Update validaciones.c

--- validaciones.c
+++ validaciones.c
@@ -5,5 +5,28 @@ bool validar_impacto(Tablero* flota, Coordenada coordenada) {
 }
 
 bool verificar_hundido(Tablero* flota, Coordenada coordenada) {
-    // Implementación de verificación de barco hundido pendiente
+    bool verificar_hundido(Tablero* flota, Coordenada coordenada) {
+    int x = coordenada.x;
+    int y = coordenada.y;
+    
+    char id_barco = obtener_id_barco(flota, x, y);
+    if (!id_barco) return false;
+
+    bool hundido = true;
+    for(int i = 0; i < flota->n && hundido; i++) {
+        for(int j = 0; j < flota->n && hundido; j++) {
+            EstadoCasilla estado = flota->casillas[i + j * flota->n];
+            if (estado == TOCADO && !es_parte_de_barco_hundido(flota, i, j, id_barco)) {
+                hundido = false;
+            }
+        }
+    }
+
+    if (hundido) {
+        marcar_barcos_hundidos(flota, id_barco);
+        marcar_casillas_contiguas_agua(flota, id_barco);
+    }
+
+    return hundido;
+    }    
 }
