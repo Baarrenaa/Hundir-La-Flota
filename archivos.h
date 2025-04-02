243d6d7ab15585c46c6883ab33f467e25f443876
Robelit <roberto11lm2018@gmail.com>
2025-04-02 14:28:21 +0200

    Update archivos.h

--- archivos.h
+++ archivos.h
@@ -1,16 +1,1 @@
-#ifndef ARCHIVOS_H
-#define ARCHIVOS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct {
-    char nombre[21];
-    char id;
-    int tamano;
-} Barco;
-
-Barco* cargar_barcos(const char* filename);
-void liberar_barcos(Barco* barcos);
-
-#endif
