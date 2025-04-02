c6c0509d269aa2630b36aaa446e0e308af845d21
Robelit <roberto11lm2018@gmail.com>
2025-04-02 14:28:31 +0200

    Update barcos.h

--- barcos.h
+++ barcos.h
@@ -1,14 +1,1 @@
-#ifndef BARCOS_H
-#define BARCOS_H
 
-#include "archivos.h"
-
-typedef struct {
-    Barco* tipos;
-    int cantidad;
-} Flota;
-
-Flota* crear_flota(Barco* tipos_barcos, int num_tipos);
-void liberar_flota(Flota* flota);
-
-#endif
