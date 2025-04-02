48b6b2b3f9272d04b35386e7a7ba24bf055f4653
Robelit <roberto11lm2018@gmail.com>
2025-04-02 14:32:07 +0200

    Update validaciones.h

--- validaciones.h
+++ validaciones.h
@@ -1,1 +1,8 @@
+#ifndef VALIDACIONES_H
+#define VALIDACIONES_H
 
+int es_posicion_valida(int fila, int columna, int tamano_tablero);
+int es_colocacion_barco_valida(Tablero* tablero, int fila, int columna, 
+                              int tamano, int orientacion);
+
+#endif
