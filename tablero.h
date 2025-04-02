1dc9c6aa410aa291d8bc3581a3dca37fce83776c
Robelit <roberto11lm2018@gmail.com>
2025-04-02 14:30:39 +0200

    Update tablero.h

--- tablero.h
+++ tablero.h
@@ -1,1 +1,13 @@
+#ifndef TABLERO_H
+#define TABLERO_H
 
+typedef struct {
+    int tamano;
+    char** casillas;
+} Tablero;
+
+Tablero* crear_tablero(int tamano);
+void inicializar_tablero(Tablero* tablero);
+void liberar_tablero(Tablero* tablero);
+
+#endif
