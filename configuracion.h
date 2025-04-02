7533d84e5708146c3759d7786d76002c1e99f29b
Robelit <roberto11lm2018@gmail.com>
2025-04-02 14:31:16 +0200

    Update configuracion.h

--- configuracion.h
+++ configuracion.h
@@ -1,1 +1,14 @@
+#ifndef CONFIGURACION_H
+#define CONFIGURACION_H
 
+typedef struct {
+    int tamano_tablero;
+    Barco* barcos;
+    int num_barcos;
+} Configuracion;
+
+Configuracion* crear_configuracion();
+void guardar_configuracion(Configuracion* config, const char* archivo);
+Configuracion* cargar_configuracion(const char* archivo);
+
+#endif
