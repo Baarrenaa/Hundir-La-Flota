30c8bbe0542def9e44661baac85900c5fb026220
Robelit <roberto11lm2018@gmail.com>
2025-04-02 13:59:56 +0200

    Update validaciones.h

--- validaciones.h
+++ validaciones.h
@@ -1,1 +1,9 @@
+#ifndef VALIDACIONES_H
+#define VALIDACIONES_H
 
+#include "configuracion.h"
+
+bool validar_impacto(Tablero* flota, Coordenada coordenada);
+bool verificar_hundido(Tablero* flota, Coordenada coordenada);
+
+#endif
