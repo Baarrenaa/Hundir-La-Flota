3ed5201e1624370cf030487e6cc0df426645784f
Robelit <roberto11lm2018@gmail.com>
2025-04-02 14:04:40 +0200

    Update validaciones.c

--- validaciones.c
+++ validaciones.c
@@ -1,1 +1,9 @@
+#include "validaciones.h"
 
+bool validar_impacto(Tablero* flota, Coordenada coordenada) {
+    return flota->casillas[coordenada.x + coordenada.y * flota->n] != VACIO;
+}
+
+bool verificar_hundido(Tablero* flota, Coordenada coordenada) {
+    // Implementación de verificación de barco hundido pendiente
+}
