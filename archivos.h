5e4809dcfb80c24021f7a3c8b92cc637f69d4210
Robelit <roberto11lm2018@gmail.com>
2025-04-02 14:31:53 +0200

    Update archivos.h

--- archivos.h
+++ archivos.h
@@ -1,1 +1,7 @@
+#ifndef ARCHIVOS_H
+#define ARCHIVOS_H
 
+int guardar_partida(const char* archivo, Configuracion* config);
+int cargar_partida(const char* archivo, Configuracion** config);
+
+#endif
