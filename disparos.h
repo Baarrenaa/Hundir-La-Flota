2d1a199c5aaa8853970c07f0afb8fa7d651fe3e4
Robelit <roberto11lm2018@gmail.com>
2025-04-02 13:58:45 +0200

    Update disparos.h

--- disparos.h
+++ disparos.h
@@ -1,1 +1,10 @@
+#ifndef DISPAROS_H
+#define DISPAROS_H
 
+#include "configuracion.h"
+#include "validaciones.h"
+
+bool realizar_disparo(Configuracion* config, Jugador* jugador_actual, 
+                     Jugador* oponente, Coordenada coordenada);
+
+#endif
