9ef668f9a8f06c94a8a1c53813238b74c1d158c8
Robelit <roberto11lm2018@gmail.com>
2025-04-02 14:29:27 +0200

    Update tablero.c

--- tablero.c
+++ tablero.c
@@ -1,13 +1,1 @@
-#include "tablero.h"
 
-Tablero* crear_tablero(int n) {
-    Tablero* tablero = malloc(sizeof(Tablero));
-    tablero->n = n;
-    tablero->casillas = calloc(n * n, sizeof(EstadoCasilla));
-    return tablero;
-}
-
-void liberar_tablero(Tablero* tablero) {
-    free(tablero->casillas);
-    free(tablero);
-}
