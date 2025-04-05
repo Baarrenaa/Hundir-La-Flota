40be3b4d86f1d1873c0fb65ca70b4a35b6066cdb
Robelit <roberto11lm2018@gmail.com>
2025-04-05 13:56:25 +0200

    Update disparos.h

--- disparos.h
+++ disparos.h
@@ -1,7 +1,6 @@
 #ifndef DISPAROS_H
 #define DISPAROS_H
 
-int realizar_disparo(Tablero* tablero_flota, Tablero* tablero_oponente, 
-                     int fila, int columna, char tipo_disparo);
+int realizar_disparo(Tablero* tablero_flota, Tablero* tablero_oponente, int fila, int columna, char tipo_disparo);
 
 #endif
