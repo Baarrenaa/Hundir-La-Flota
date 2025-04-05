db7754f8669bdff42b1aec9d1291466a68a01f69
Robelit <roberto11lm2018@gmail.com>
2025-04-05 13:50:17 +0200

    Update validaciones.h

--- validaciones.h
+++ validaciones.h
@@ -17,7 +17,6 @@ int es_colocacion_barco_valida(Tablero* tablero, int fila, int columna,
 int es_tipo_disparo_valido(char tipo);
 int es_nombre_jugador_valido(const char* nombre);
 int es_tamano_tablero_valido(int tamano);
-int es_cantidad_barcos_valida(int cantidad, int tamano_barco, 
-                            int tamano_tablero);
+int es_cantidad_barcos_valida(int cantidad, int tamano_barco, int tamano_tablero);
 
 #endif
