03fb356fbbf6329244791c50fd41b34dac7e2091
Robelit <roberto11lm2018@gmail.com>
2025-04-09 13:03:36 +0200

    Update archivos.c

--- archivos.c
+++ archivos.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "archivos.h"
+
+int guardar_partida(const char* archivo, Configuracion* config) {
+    FILE* fichero = NULL;
+    
+    fichero = fopen(archivo, "w");
+    if (!fichero) {
+        fprintf(stderr, "Error al guardar la partida en %s\n", archivo);
+        return 0;
+    }
+    
 
